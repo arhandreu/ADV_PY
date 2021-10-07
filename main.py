9e22b4b7b692a76be7bf9e488c5f4f4cd754156f
Andreu Bardukov <arhandreu@gmail.com>
2021-10-07 10:12:03 +0500

    Revert "ДЗ_1 Вариант_1"
    This reverts commit 1efc77f29d6faae9a8c5e7ab1acf2fc409c9ea40.

--- main.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-if __name__ == '__main__':
-    print(f'Дата: {date.today()}')
-    calculate_salary()
-    get_employees()
-
